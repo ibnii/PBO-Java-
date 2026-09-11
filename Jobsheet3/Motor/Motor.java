56f63fae11448af2b82afb5cec9ca022039decac
Ibni Andarta <ibniandarta9999@gmail.com>
2026-09-11 08:33:43 +0700

    3.2 Percobaan 2

--- Jobsheet3/Motor/Motor.java
+++ Jobsheet3/Motor/Motor.java
@@ -1,14 +1,40 @@
 package Motor;
+
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakon = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena Mesin Off! \n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena Mesin Off! \n");
+        }
+    }
 
     public void printStatus() {
-        if (kontakon == true) {
+        if (kontakOn == true) {
             System.out.println("Kontak On");
         } else {
             System.out.println("Kontak Off");
-            System.out.println("Kecepatan " + kecepatan + "\n");
         }
+        System.out.println("Kecepatan " + kecepatan + "\n");
     }
 }
--- Jobsheet3/Motor/MotorDemo.java
+++ Jobsheet3/Motor/MotorDemo.java
@@ -1,9 +1,24 @@
 package Motor;
+
 public class MotorDemo {
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
         motor.printStatus();
     }
 }
