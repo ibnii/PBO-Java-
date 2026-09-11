85e91e3e4f7e9a15b8d897788f48911a23e3a043
Ibni Andarta <ibniandarta9999@gmail.com>
2026-09-11 08:28:47 +0700

    3.1 Percobaan 1

---
+++ Jobsheet3/Motor/Motor.java
@@ -0,0 +1,14 @@
+package Motor;
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakon = false;
+
+    public void printStatus() {
+        if (kontakon == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+            System.out.println("Kecepatan " + kecepatan + "\n");
+        }
+    }
+}
