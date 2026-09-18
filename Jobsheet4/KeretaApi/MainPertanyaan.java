a606a0115accbf56e57824c78ed9384f29a0b2b2
Ibni Andarta <ibniandarta9999@gmail.com>
2026-09-18 08:36:30 +0700

    Percobaan 3

---
+++ Jobsheet4/KeretaApi/MainPertanyaan.java
@@ -0,0 +1,9 @@
+package Jobsheet4.KeretaApi;
+
+public class MainPertanyaan {
+    public static void main(String[] args) {
+        Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
+        KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis);
+        System.out.println(keretaApi.info());
+    }
+}
